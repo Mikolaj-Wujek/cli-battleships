6d9aae45ae62936a5f4fd375a4f6b4df04ea5812
mikolaj-wujek2 <mikolajwork1@gmail.com>
2026-06-10 21:48:42 +0100

    added shot tracker

--- client.py
+++ client.py
@@ -4,9 +4,11 @@ import json
 import threading
 from rich.table import Table
 from rich import print
+from rich.columns import Columns
 
 placement_phase = threading.Event()
 ships = ["carrier","battleship","cruiser","submarine","destroyer"]
+shot_board = [["•" for _ in range(10)] for _ in range(10)]
 
 placement_response = threading.Event()
 last_result = [None]         #RETURN TO THIS
@@ -17,6 +19,9 @@ my_turn = threading.Event()
 shot_response = threading.Event()
 last_shot_result = [None]
 
+last_message = " "
+my_grid = [None]
+
 client_socket = socket(AF_INET, SOCK_STREAM)
 server_name = input("input the IP address to be used: ")
 server_port = int(input("enter the port number to be used: "))
@@ -25,19 +30,30 @@ server_address = (server_name, server_port)
 print('connecting to server at %s port %s' % server_address)
 client_socket.connect(server_address)
 
-def display(grid):
-    table = Table()
-    table.add_column(" ")  # for row letters
+def display(grid, second_grid = None):
+    table1 = Table(title="My Board")
+    table1.add_column(" ")  # for row letters
     for i in range(1, 11):
-        table.add_column(str(i))
+        table1.add_column(str(i))
     
     letters = "ABCDEFGHIJ"
     for i in range(10):
-        table.add_row(letters[i], *grid[i])
-    
-    print(table)
+        table1.add_row(letters[i], *grid[i])
+
+    if second_grid:
+        table2 = Table(title="Shot Tracker")
+        table2.add_column(" ")
+        for i in range(1, 11):
+            table2.add_column(str(i))
+        for i in range(10):
+            table2.add_row(letters[i], *second_grid[i])
+        print(Columns([table1, table2]))
+    else:
+        print(table1)
 
 def listen():
+    global last_message
+    global my_grid
     while True:
         try:
             data = client_socket.recv(4096)
@@ -46,10 +62,12 @@ def listen():
                 print(msg["content"])
             elif msg["type"] == "start_placing":
                 display(msg["grid"])
+                my_grid = msg["grid"]
                 placement_phase.set()
             elif msg["type"] == "place_result":
                 last_result[0] = msg["content"]
-                display(msg["grid"])
+                display(msg["grid"])  
+                my_grid = msg["grid"]
                 placement_response.set()
             elif msg["type"] == "onedone":
                 print(msg["content"])
@@ -60,10 +78,12 @@ def listen():
                 print("Your turn")
                 if "grid" in msg:
                     display(msg["grid"])
+                    my_grid = msg["grid"]
                 my_turn.set()
             elif msg["type"] == "shot_result":
                 last_shot_result[0] = msg["content"]
                 print(msg["message"])
+                last_message = msg["message"]
                 shot_response.set()
 
 
@@ -103,6 +123,10 @@ while True:
         client_socket.send(json.dumps(msg).encode())
         shot_response.wait()
         shot_response.clear()
+    row = ord(shot[0].upper()) - ord('A')
+    col = int(shot[1:]) - 1
+    shot_board[row][col] = "X" if last_message == "hit" else "O"
+    display(my_grid, shot_board)
     my_turn.clear()
 
 
